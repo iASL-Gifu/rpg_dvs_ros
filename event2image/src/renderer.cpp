e0fd6a52a2052d2f054610b3e53cf5a909f92fc4
Tomoki <kunotomoki61@gmail.com>
2023-10-30 18:29:09 +0900

    Done:: events2image

--- event2image/src/renderer.cpp
+++ event2image/src/renderer.cpp
@@ -14,40 +14,51 @@
 #include <dvs_msgs/EventArray.h>
 #include <std_msgs/Float32.h>
 
+class SubPub
+{
+    private:
+	ros::NodeHandle nh;
+        ros::Publisher  image_pub;
+        ros::Subscriber event_sub;   
+    public:
+        void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg)
+        {
+          cv_bridge::CvImage cv_image;
+          if (msg->events.size() > 0)
+          {
+            cv_image.header.stamp = msg->events[msg->events.size()/2].ts;
+          }
+          //Make image
+          cv_image.encoding = "bgr8";
+          cv_image.image = cv::Mat(msg->height, msg->width, CV_8UC3);
+          cv_image.image = cv::Scalar(0,0,0);
+        
+          for (int i = 0; i < msg->events.size(); ++i)
+          {
+            const int x = msg->events[i].x;
+            const int y = msg->events[i].y;
+        
+            cv_image.image.at<cv::Vec3b>(cv::Point(x, y)) = (
+                msg->events[i].polarity == true ? cv::Vec3b(255, 0, 0) : cv::Vec3b(0, 0, 255));
+          }
+          image_pub.publish(cv_image.toImageMsg());
+        }
+        
+        SubPub()
+        {
+          image_pub = nh.advertise<sensor_msgs::Image>("eventsImage", 1);
+          event_sub = nh.subscribe("/dvs/events", 1, &SubPub::eventsCallback, this);
+        }
+
+};
+
+
+
 int main(int argc, char** argv)
 {
 	ros::init(argc,argv, "event2image");
-	ros::NodeHandle nh;
-        // setup subscribers and publishers
-        ros::Subscriber event_sub = nh.subscribe("events", 1, &eventsCallback);
-        ros::Publisher  image_pub = nh.advertise("eventsImage", 1);
-
+        
+        SubPub subpub;
 	ros::spin();
 	return 0;
 }
-
-
-void eventsCallback(const dvs_msgs::EventArray::ConstPtr& msg)
-{
-  cv_bridge::CvImage cv_image;
-  if (msg->events.size() > 0)
-  {
-    cv_image.header.stamp = msg->events[msg->events.size()/2].ts;
-  }
-  //Make image
-  cv_image.encoding = "bgr8";
-  cv_image.image = cv::Mat(msg->height, msg->width, CV_8UC3);
-  cv_image.image = cv::Scalar(0,0,0);
-
-  for (int i = 0; i < msg->events.size(); ++i)
-  {
-    const int x = msg->events[i].x;
-    const int y = msg->events[i].y;
-
-    cv_image.image.at<cv::Vec3b>(cv::Point(x, y)) = (
-        msg->events[i].polarity == true ? cv::Vec3b(255, 0, 0) : cv::Vec3b(0, 0, 255));
-  }
-  image_pub.publish(cv_image.toImageMsg());
-}
-
-
